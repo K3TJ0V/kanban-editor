fba5e9e269f91d727531adc21186fef2de5cb851
Wojciech Górka <wojowany@gmail.com>
2023-04-15 15:24:51 +0200

    background and hamburger with first link

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 import "normalize.css";
 import "../scss/main.scss";
 
-console.log("test");
+const header = document.querySelector('.header');
+const placeholder = document.querySelector('.placeholder')
+const hamburger = document.querySelector('.header__hamburger--js');
+const navBlock = document.querySelector('.header__nav--js');
+
+hamburger.addEventListener('click', ()=>{
+    navBlock.classList.toggle('visible');
+    navBlock.prepend(hamburger);
+    header.prepend(placeholder)
+    if(navBlock.classList == "header__nav header__nav--js"){
+        header.prepend(hamburger);
+        navBlock.prepend(placeholder)
+    }
+})
